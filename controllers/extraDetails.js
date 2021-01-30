ae2b7cc510c9ae00072198d7115089b76618d85d
Waryserpant122 <yashshah123boy@gmail.com>
2021-01-31 01:21:38 +0530

    [add] : Added sort-details-extra 2

--- controllers/extraDetails.js
+++ controllers/extraDetails.js
@@ -10,17 +10,17 @@ exports.getExtraDetails = async(req, res, next) => {
         const title = details.title
 
         const list = await Student.find({
-                "history.extraCurricular.tag": tag,
-                "history.extraCurricular.title": title,
-                "gender": gender
-            }).sort({ "history.extraCurricular.rating": 1 })
-            //await student.save();
+            "history.extraCurricular.tag": tag,
+            "history.extraCurricular.title": title,
+            "gender": gender,
+            "history.collegeId": collegeId,
+        }).sort({ "history.extraCurricular.rating": 1 })
         res.status(201).json({
             message: "Post created successfully!",
             list: list,
-            details: details
         });
     } catch (err) {
-        res.status(500).send("SOMETHING WENT WRONG!!");
+        if (err.message) return res.status(500).json({ message: err.message });
+        else return res.status(500).json({ message: "Something Went Wrong!" });
     }
 };
