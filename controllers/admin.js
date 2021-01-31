c685c8ca9d9c4e91cb854339931f7a12f34e7c39
Subhradeep Saha <subhradeep11012000@gmail.com>
2021-01-31 11:38:19 +0530

    fix: drop api

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,16 +1,13 @@
 const Student = require('../models/student')
 
 const getDroppedStudents = async (req, res, next) => {
-    let students = await Student.find({})
+    let students = await Student.find({ goingClass: { $lt: 13 } })
     let dropped_students = []
     const currYear = new Date().getFullYear()
     
     for(let student of students){
         let studHist = student.history
-        if (studHist.length === 0){
-            continue;
-        }
-        else {
+        if (studHist.length !== 0){
             if(studHist[studHist.length - 1].year !== currYear){
                 dropped_students.push({
                     student
